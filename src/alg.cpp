d01c37c743b91b2efe4c4a68d6872a3fc53ced65
pikaty4 <71702322+pikaty4@users.noreply.github.com>
2022-06-05 22:25:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,29 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include <string>
 #include  "bst.h"
 
-BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST<std::string> makeTree(const char* path) {
+      BST<std::string> tree;
+    std::ifstream file(path);
+    if (!file) {
+        std::cout << "can't read file" <<"\n";
+        throw std::string("can't open file!");
+    }
+    std::string word = "";
+    while (!file.eof()) {
+        char IdontWantToDoIt = file.get();
+        int sym = IdontWantToDoIt;
+        if ((sym >= 65 && sym <= 90) || (sym >= 97 && sym <= 122)) {
+            word += tolower(sym);
+        } else { if (word != "") {
+                tree.Add(word);
+                word = "";
+            }
+        }
+    }
+    file.close();
+    return tree;
 }
